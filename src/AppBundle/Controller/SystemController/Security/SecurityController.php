cfe2d147f59f6030891ca2cbeaf033fe84b705c7
Wargnier <crh.wargnier@gmail.com>
2016-10-27 15:08:24 +0200

    update comment

--- src/AppBundle/Controller/SystemController/Security/SecurityController.php
+++ src/AppBundle/Controller/SystemController/Security/SecurityController.php
@@ -12,14 +12,11 @@ class SecurityController extends Controller {
      * @Route("/login", name="login",options={"expose"=true})
      */
     public function loginAction(Request $request) {
-        // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+        //redirection si l'user est déjà connecté
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
             return $this->redirectToRoute('home');
         }
 
-        // Le service authentication_utils permet de récupérer le nom d'utilisateur
-        // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
-        // (mauvais mot de passe par exemple)
         $authenticationUtils = $this->get('security.authentication_utils');
 
         return $this->render('Public/form.login.html.twig', array(
