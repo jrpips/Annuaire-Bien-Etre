81500437d10546d6ac67a60e851b533255a4934e
Wargnier <crh.wargnier@gmail.com>
2016-11-22 15:39:43 +0100

    gestion categ services

---
+++ src/AppBundle/Form/CategServiceType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Form\ImageType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class CategServiceType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, array('required' => false))
+            ->add('description', TextareaType::class, array('required' => false))
+            ->add('enAvant', CheckboxType::class, array('required' => false))
+            //->add('valide', CheckboxType::class, array('required' => false))
+            //->add('prestataires')
+            ->add('image', ImageType::class, array('required' => false))
+            ->add('Envoyer',SubmitType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\CategService'
+        ));
+    }
+}
