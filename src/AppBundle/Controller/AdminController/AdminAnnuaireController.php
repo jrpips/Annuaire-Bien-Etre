17c2d703eab705055ad46226e63f3cd59c33a971
Wargnier <crh.wargnier@gmail.com>
2017-01-16 16:44:01 +0100

    86

--- src/AppBundle/Controller/AdminController/AdminAnnuaireController.php
+++ src/AppBundle/Controller/AdminController/AdminAnnuaireController.php
@@ -4,9 +4,19 @@ namespace AppBundle\Controller\AdminController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Finder\Finder;
+
+use AppBundle\Entity\Prestataire;
+use AppBundle\Entity\Image;
+use AppBundle\Entity\Utilisateur;
 use AppBundle\Entity\Commentaire;
 use AppBundle\Entity\Abus;
+
+use AppBundle\Form\UtilisateurType;
+use AppBundle\Form\PrestataireType;
+use AppBundle\Form\InternauteType;
 
 class AdminAnnuaireController extends Controller
 {
@@ -16,7 +26,7 @@ class AdminAnnuaireController extends Controller
     public function gestionAbusAction()
     {
         $abus = $this->getDoctrine()->getManager()->getRepository('AppBundle:Abus')->findAll();
-        dump($abus);
+
         return $this->render('Admin/GestionAbus/dashboard.abus.html.twig', array(
             'abus' => $abus
         ));
@@ -28,7 +38,7 @@ class AdminAnnuaireController extends Controller
     public function gestionCompteBannisAction()
     {
         $users = $this->getDoctrine()->getManager()->getRepository('AppBundle:Utilisateur')->findBannedUsers();
-        dump($users);
+
         return $this->render('Admin/GestionBannedAccount/dashboard.banned.account.html.twig', array(
             'users' => $users
         ));
@@ -41,6 +51,7 @@ class AdminAnnuaireController extends Controller
     public function gestionCommentairesAction()
     {
         $commentaires = $this->getDoctrine()->getManager()->getRepository('AppBundle:Commentaire')->getAllCommentaires();
+
         return $this->render('Admin/GestionCommentaires/dashboard.gestion.commmentaires.html.twig', array(
             'commentaires' => $commentaires
         ));
@@ -54,8 +65,9 @@ class AdminAnnuaireController extends Controller
         $user = $this->getDoctrine()->getManager()->getRepository('AppBundle:Utilisateur')->findOneById($user_id);
         $em = $this->getDoctrine()->getManager();
         $user->setBanni(false);
-        $em->persist($user);
-        $em->flush();
+
+        $em->persist($user)->flush();
+
         return $this->redirectToRoute('dashboard_banned_account');
     }
 
@@ -76,26 +88,205 @@ class AdminAnnuaireController extends Controller
     /**
      * @Route("/admin/supprimer/commentaire/{commentaire}/{abus}/{banni}",options={"expose"=true},name="delete_comment")
      */
-    public function deleteCommentAction(Request $request,Commentaire $commentaire,Abus $abus=null, $banni = null)
+    public function deleteCommentAction(Request $request, Commentaire $commentaire, Abus $abus = null, $banni = null)
     {
-        //$commentaire = $this->getDoctrine()->getManager()->getRepository('AppBundle:Commentaire')->findOneByTitre($commentaire_titre);
-        //$abus = $this->getDoctrine()->getManager()->getRepository('AppBundle:Abus')->findOneById($abus_id);
         if ($banni) {
+
+
             $user = $this->getDoctrine()->getManager()->getRepository('AppBundle:Utilisateur')->findUtilisateur($banni);
+
         }
 
         $em = $this->getDoctrine()->getManager();
 
         if ($banni) {
+
+
             $user[0]->setBanni(true);
             $em->persist($user[0]);
+
         }
-        if($abus){
-        $em->remove($abus);
-        $em->flush();}
+        if ($abus) {
+
+
+            $em->remove($abus);
+            $em->flush();
+
+        }
+
         $em->remove($commentaire);
         $em->flush();
 
         return $this->redirectToRoute('dashboard_abus');
     }
+
+    /**
+     * @Route("/admin/liste/{type}",name="ad_utilisateurs")
+     */
+    public function listePrestatairesAction($type)
+    {
+        if ($type == 'prestataire') {
+
+
+            $utilisateurs = $this->getDoctrine()->getManager()->getRepository('AppBundle:Prestataire')->findAll();
+
+        } else {
+
+
+            $utilisateurs = $this->getDoctrine()->getManager()->getRepository('AppBundle:Internaute')->findUtilisateurs();
+
+        }
+
+        return $this->render('Admin/GestionUtilisateurs/dashboard.gestion.utilisateurs.html.twig', array(
+            'utilisateurs' => $utilisateurs,
+            'type' => $type
+        ));
+    }
+
+    /**
+     * @Route("/admin/utilisateur/edition/{utilisateur}/{type}",name="ad_edit_utilisateur")
+     */
+    public function editPrestataireAction(Request $request, Utilisateur $utilisateur, $type)
+    {
+        if ($type == 'prestataire') {
+
+
+            $form = $this->get('form.factory')->create(UtilisateurType::class, $utilisateur)->add('prestataire', PrestataireType::class);
+
+        } else {
+
+
+            $form = $this->get('form.factory')->create(UtilisateurType::class, $utilisateur)->add('internaute', InternauteType::class);
+        }
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $em = $this->getDoctrine()->getManager();
+
+            $statut = 'success';
+            $text = 'du ' . $type . ' ' . $utilisateur->getUsername();
+
+            try {
+
+
+                $em->flush();
+
+            } catch (\Exception $e) {
+
+
+                $statut = 'danger';
+
+            }$this->get('app.addmsgflash')->addMsgFlash($request, $statut, $text);
+
+            return $this->redirectToRoute('ad_utilisateurs', array('type' => $type));
+
+        }
+        return $this->render('Admin/GestionUtilisateurs/Links/edit.utilisateur.html.twig', array(
+            'utilisateur' => $utilisateur,
+            'form' => $form->createView(),
+            'type' => $type
+        ));
+    }
+
+    /**
+     * @Route("/admin/utilisateur/bannir/{utilisateur}/{type}",name="ban_utilisateur")
+     */
+    public function banUtilisateurAction(Request $request, Utilisateur $utilisateur, $type)
+    {
+        $utilisateur->setBanni(true);
+
+        $em = $this->getDoctrine()->getManager();
+
+        $statut = 'success';
+        $text = "L'utilisateur " . $utilisateur->getUsername() . " est à présent banni!";
+
+        try {
+
+
+            $em->flush();
+
+        } catch (\Exception $e) {
+
+
+            $statut = 'danger';
+            $text = "Une erreur est survenue lors de l'action demandée!";
+
+        }
+        $this->get('app.addmsgflash')->addMsgFlash($request, $statut, $text, true);
+
+        return $this->redirectToRoute('ad_utilisateurs', array('type' => $type));
+    }
+
+    /**
+     * @Route("/admin/contacter/utilisateur/{utilisateur}",name="contact_user")
+     */
+    public function contactUserAction(Request $request, Utilisateur $utilisateur = null)
+    {
+
+        $values = $request->request->get('contact_user');
+        $error = '';
+
+        if ($request->getMethod() == 'POST') {
+
+
+            if ($values['message']) {
+
+
+                $values['contact']['email'] = $utilisateur->getEmail();
+
+                $this->get('app.mailerbuilder')->mailConstruct($values, 'admin', 'from');
+
+                $this->get('app.addmsgflash')->addMsgFlash($request, 'success', 'Message envoyé!', true);
+
+                return $this->redirectToRoute('dashboard_accueil');
+
+            } else {
+
+
+                $error = "Votre message est vide!";
+            }
+        }
+
+        return $this->render('Admin/GestionUtilisateurs/Links/contact.utilisateur.html.twig', array(
+            'utilisateur' => $utilisateur,
+            'error' => $error
+        ));
+    }
+
+    /**
+     * @Route("/admin/gestion/images",name="image_dashboard")
+     */
+    public function gestionImageAction()
+    {
+        $finder = new Finder();
+        $finder->files()->in(__DIR__ . '/../../../../web/image/userUploads');
+
+        foreach ($finder as $file) {
+
+            $files[] = '/image/userUploads/' . $file->getRelativePathname();
+        }
+
+        return $this->render('Admin/GestionImages/finder.html.twig', array(
+            'finder' => $files,
+
+        ));
+    }
+
+    /**
+     * @Route("/admin/infos/image",options={"expose"=true},name="get_infos_img")
+     */
+    public function autoCompleteAjaxAction(Request $request) {
+        if ($request->getMethod() == 'POST' && $request->isXmlHttpRequest()) {
+
+
+            $val = $request->request->get('path');
+            $response = $this->getDoctrine()->getManager()->getRepository('AppBundle:Image')->findOneByPath($val);
+
+            return new JsonResponse($response);
+        }
+    }
+
 }
